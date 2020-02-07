87d3b78eaf3a5b6d48d7a37727bc0b13edadeb14
Vishal Mishra <vishalm434@gmail.com>
2020-02-07 12:01:44 +0530

    Add url endpoint for form submission

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
                 path('', views.home, name='home'),
+                path('submit', views.submission_form, name='submission_form'),
             ]
--- recruitment/views.py
+++ recruitment/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recruitment/index.html',{})
+
+def submission_form(request):
+    if request.method == 'POST':
+        print('Data! got')
+    return render(request, 'recruitment/form.html', {})
