f4a093d7fee7b7e68ad3099ec27c67122ae74593
Vishal Mishra <vishalm434@gmail.com>
2020-02-21 14:41:46 +0530

    Update models for null and blank values

--- recruitment/models.py
+++ recruitment/models.py
@@ -101,6 +101,8 @@ class Teaching_and_research_detail(models.Model):
     applicant = models.ForeignKey(Applicant, 
                                 on_delete=models.CASCADE,
                                 related_name='teaching_and_research_details',
+                                null=True,
+                                blank=True,
                 )
     course_title = models.CharField(max_length=200)
     level = models.CharField(max_length=2,
@@ -108,32 +110,38 @@ class Teaching_and_research_detail(models.Model):
                                 ('UG', 'Undergraduate'),
                                 ('PG', 'Postgraduate'),
                                 ],
+                            null=True,
+                            blank=True,
             )
     sole_instructor = models.CharField(max_length=1,
                                         choices=[
                                             ('Y','Yes'),
                                             ('N','No'),
                                             ],
+                                        null=True,
+                                        blank=True,
             )
     developer_of_course = models.CharField(max_length=1,
                                             choices=[
                                                 ('Y','Yes'),
                                                 ('N','No'),
                                                 ],
+                                            null=True,
+                                            blank=True,
             )
-    name_of_student = models.CharField(max_length=200)
-    year_of_completion = models.IntegerField()
-    name_of_institute = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
-    guide = models.CharField(max_length=200)
-    period = models.CharField(max_length=200)
-    organisation = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
-    amount = models.IntegerField()
-    co_investigators = models.CharField(max_length=200)
-    period = models.CharField(max_length=100)
-    name_of_body = models.CharField(max_length=150)
-    status_of_membership = models.CharField(max_length=150)
+    name_of_student = models.CharField(max_length=200, null=True)
+    year_of_completion = models.IntegerField(null=True, blank=True)
+    name_of_institute = models.CharField(max_length=200, null=True, blank=True)
+    title = models.CharField(max_length=200, null=True, blank=True)
+    guide = models.CharField(max_length=200, null=True, blank=True)
+    period = models.CharField(max_length=200, null=True, blank=True)
+    organisation = models.CharField(max_length=200, null=True, blank=True)
+    title = models.CharField(max_length=200, null=True, blank=True)
+    amount = models.IntegerField(null=True, blank=True)
+    co_investigators = models.CharField(max_length=200, null=True, blank=True)
+    period = models.CharField(max_length=100, null=True, blank=True)
+    name_of_body = models.CharField(max_length=150, null=True, blank=True)
+    status_of_membership = models.CharField(max_length=150, null=True, blank=True)
 
     def __str__(self):
         return str(self.applicant.name)
