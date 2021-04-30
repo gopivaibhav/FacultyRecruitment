7c5e8fe5bb4242913dfb8f62fa87ff44ea7a6975
Cronus1007 <lit2019058@iiitl.ac.in>
2021-04-30 18:51:30 +0530

    added login page and url

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-                path('', views.home, name='home'),
+                path('', views.login, name='login'),
+                path('home/', views.home, name='home'),
                 path('submit/', views.submission_form, name='submission_form'),
                 path('submit/admin/',views.admin,name="Admin"),
             ]
