eb22187f438f7ce498f1fed36ed5e169ee6ec9a8
Cronus1007 <lit2019058@iiitl.ac.in>
2021-05-03 16:33:32 +0530

    message

--- recruitment/views.py
+++ recruitment/views.py
@@ -32,7 +32,7 @@ def submission_form(request):
         applicant_data['post'] = data['AppPost']
         applicant_data['department'] = data['Dept']
         applicant_data['Research_Domain'] = data['research_domain']
-        applicant_data['profile_picture'] = data['profileImage']
+        applicant_data['profile_picture'] = request.FILES['profile_photo']
         Applicant.objects.create(**applicant_data)
         # General
         general_data={}
@@ -47,12 +47,13 @@ def submission_form(request):
         general_data['pin_mail'] = data['mailingpincode']
         general_data['mobile_number'] = data['mobilecode'] + data['mobile']
         general_data['email'] = data['email']
-        general_data['gender'] = data['inlineRadioOptions']
+        general_data['gender'] = data['gender']
         general_data['marital_status'] = data['maritalstatus']
         general_data['nationality'] = data['nationality']
         general_data['state'] = data['domicile_state']
         general_data['category'] = data['category']
-        general_data['reservation'] = data['reservation_certificate']
+        general_data['reservation'] = data['reservation']
+        general_data['reservation_certificate'] = request.FILES['reservation_certificate']
         general_data['present_employer'] = data['present_employer']
         general_data['applicant'] = Applicant.objects.get(application_no=application_number) 
         General.objects.create(**general_data)
@@ -65,9 +66,18 @@ def submission_form(request):
         otherinformation_data['college_punishment'] = data['miscTa5']
         otherinformation_data['judicial_punishment'] = data['miscTa6']
         otherinformation_data['unfit_for_position'] = data['miscTa7']
-        otherinformation_data['references'] = data['miscTa8']
+        otherinformation_data['reference1'] = data['miscTa8']
+        otherinformation_data['reference2'] = data['miscTa9']
+        otherinformation_data['fulfillment'] = data['miscTa10']
         otherinformation_data['applicant'] = Applicant.objects.get(application_no=application_number) 
         OtherInfo.objects.create(**otherinformation_data)
+        # Signature 
+        signed_data= {}
+        signed_data['place'] = data['signPlace']
+        signed_data['date'] = data['signDate']
+        signed_data['signature'] = request.FILES['signUpload']
+        signed_data['applicant'] = Applicant.objects.get(application_no=application_number)
+        Declaration.objects.create(**signed_data)
         # Page 4
         # Thesis
         thesis_data = {}
@@ -77,7 +87,7 @@ def submission_form(request):
         Thesis.objects.create(**thesis_data)
         # Administrative Details
         administrative_details_data = {}
-        administrative_details_data['# Administrative Details'] = data['administrative_details']
+        administrative_details_data['administrative_details'] = data['administrative_details']
         administrative_details_data['applicant'] = Applicant.objects.get(application_no=application_number)
         AdministrativeDetails.objects.create(**administrative_details_data)
         # Patent
@@ -85,15 +95,10 @@ def submission_form(request):
         patent_data['patent_details'] = data['patent_details']
         patent_data['applicant'] = Applicant.objects.get(application_no=application_number)
         Patent.objects.create(**patent_data)
-        # Membership Details
-        membership_data = {}
-        membership_data['member_details'] = data['member_details']
-        membership_data['applicant'] = Applicant.objects.get(application_no=application_number)
-        Membership.objects.create(**membership_data)
         # Summary
         summary_data = {}
         summary_data['defence_date'] = data['phd_defence_date']
-        summary_data['total_exp'] = data['phd_defence_date']
+        summary_data['total_exp'] = data['tot_exp']
         summary_data['exp_post_phd'] = data['exp_post_phd']
         summary_data['total_phd_students'] = data['total_phd_students']
         summary_data['ongoing_phd_supervision'] = data['ongoing_phd_supervision']
@@ -108,7 +113,8 @@ def submission_form(request):
         sponsored_projects_data = {}
         sponsored_projects_data['spo_tot_number'] = data['spo_tot_number']
         sponsored_projects_data['spo_ongoing'] = data['spo_ongoing']
-        sponsored_projects_data['spo_completed'] = data['spo_tot_number']
+        sponsored_projects_data['spo_completed'] = data['spo_completed']
+        sponsored_projects_data['spo_file'] = request.FILES['spofile']
         sponsored_projects_data['applicant'] = Applicant.objects.get(application_no=application_number)
         SponsoredProject.objects.create(**sponsored_projects_data)
         # Experiments
@@ -116,6 +122,7 @@ def submission_form(request):
         experiments_data['exp_tot_number'] = data['exp_tot_number']
         experiments_data['exp_ongoing'] = data['exp_ongoing']
         experiments_data['exp_completed'] = data['exp_completed']
+        experiments_data['exp_file'] = request.FILES['expfile']
         experiments_data['applicant'] = Applicant.objects.get(application_no=application_number)
         Experiments.objects.create(**experiments_data)
         # PHD
@@ -124,17 +131,27 @@ def submission_form(request):
         phd_data['title_of_thesis'] = data['phdThesis']
         phd_data['applicant'] = Applicant.objects.get(application_no=application_number)
         PhD.objects.create(**phd_data)
+        # Resarch Experience
+        research_exp_details = {}
+        research_exp_details['From'] = data['exp-from']
+        research_exp_details['To'] = data['exp-to']
+        research_exp_details['year'] = data['exp-year']
+        research_exp_details['month'] = data['exp-month']
+        research_exp_details['supporting_documents'] = request.FILES['exp-file']
+        research_exp_details['applicant'] = Applicant.objects.get(application_no=application_number)
+        ResearchExp.objects.create(**research_exp_details)
+                                
         # Academic Details
-        num_of_academic_records = len(list(filter(lambda s: 'mark' in s,list(data.keys()))))
+        num_of_academic_records = len(list(filter(lambda s: 'course' in s,list(data.keys()))))
         academic_data = []
         for i in range(1,num_of_academic_records+1):
             academic_details = {}
-            academic_details['degree'] = data.get('course-'+str(i)+'-degree',False)
-            academic_details['name'] = data.get('course-'+str(i)+'-name',False)
-            academic_details['marks'] = data.get('course-'+str(i)+'-marks',False)
-            academic_details['subjects'] = data.get('course-'+str(i)+'-subject',False)
+            academic_details['degree'] = data.get('course'+str(i),False)
+            academic_details['name'] = data.get('course'+str(i)+'-name',False)
+            academic_details['marks'] = data.get('course'+str(i)+'-marks',False)
+            academic_details['subjects'] = data.get('course'+str(i)+'-subject',False)
             academic_details['year_of_passing'] = data.get('yearOfPassing-'+str(i),False)
-            academic_details['supporting_documents'] = data.get('course-'+str(i)+'-file',False)
+            academic_details['supporting_documents'] = request.FILES['course'+str(i)+'-file']
             academic_details['applicant'] = Applicant.objects.get(application_no=application_number)
             if re.search(r'[12]\d{3}',data.get('yearOfPassing-'+str(i),False)) is None:
                 return render(request, 'recruitment/form.html',{'message':'Enter the year in Academic details in yyyy format.'})
@@ -142,18 +159,19 @@ def submission_form(request):
                 academic_details['marks']  = data.get('course-'+str(i)+'-marks',False)
             else:
                 return render(request, 'recruitment/form.html',{'message':'Enter your percentage in Academic details a value between 0 to 100.'})
-            EducationalQualifications.objects.create(**academic_details)    
+            academic_data.append(academic_details)    
         # Professional
         num_of_professional_records = len(list(filter(lambda s: 'org' in s, list(data.keys()))))
         professional_data =  []
         for i in range(1,num_of_professional_records+1):
             professional_details = {}
-            professional_details['name'] = data.get('org-' + str(i) + '-name',False)
-            professional_details['post'] = data.get('org-' + str(i) + '-post',False)
-            professional_details['from_year'] = data.get('org-' + str(i) + '-from',False)
-            professional_details['to_year'] = data.get('org-' + str(i) + '-to',False)
-            professional_details['salary'] = data.get('org-' + str(i) + '-salary',False)
-            professional_details['nature'] = data.get('org-' + str(i) + '-nature',False)
+            professional_details['name'] = data.get('org' + str(i) + '-name',False)
+            professional_details['post'] = data.get('org' + str(i) + '-post',False)
+            professional_details['from_year'] = data.get('org' + str(i) + '-from',False)
+            professional_details['to_year'] = data.get('org' + str(i) + '-to',False)
+            professional_details['salary'] = data.get('org' + str(i) + '-salary',False)
+            professional_details['nature'] = data.get('org' + str(i) + '-nature',False)
+            professional_details['supporting_documents'] = request.FILES['org'+str(i)+'-file']
             professional_details['applicant'] = Applicant.objects.get(application_no=application_number)
             EmploymentExp.objects.create(**professional_details)
 
@@ -162,51 +180,58 @@ def submission_form(request):
         books_data = []
         for i in range(1,num_of_books_records+1):
             books_details = {}
-            books_details['title'] = data.get('books-' + str(i) + '-title',False)
-            books_details['author'] = data.get('books-' + str(i) + '-author',False)     
-            books_details['publisher'] = data.get('books-' + str(i) + '-publisher',False) 
-            books_details['date_of_publisher'] = data.get('books-' + str(i) + '-date',False)
-            books_details['isbn_issn'] = data.get('books-' + str(i) + '-number',False)  
+            books_details['title'] = data.get('books' + str(i) + '-title',False)
+            books_details['author'] = data.get('books' + str(i) + '-author',False)     
+            books_details['publisher'] = data.get('books' + str(i) + '-publisher',False) 
+            books_details['date_of_publisher'] = data.get('books' + str(i) + '-date',False)
+            books_details['isbn_issn'] = data.get('books' + str(i) + '-number',False)
+            books_details['supporting_documents'] = request.FILES['books'+str(i)+'-file']
             books_details['applicant'] = Applicant.objects.get(application_no=application_number)
-            Books.objects.create(**books_details)
+            books_data.append(books_details)
         # Chapters
         num_of_chapters_records = len(list(filter(lambda s: 'chapters' in s, list(data.keys()))))    
         chapters_data = []
         for i in range(1,num_of_chapters_records+1):
             chapter_details = {}
-            chapter_details['book_title'] = data.get('chapters-' + str(i) + '-book_title',False)
-            chapter_details['chapter'] = data.get('chapters-' + str(i) + '-chapter',False)    
-            chapter_details['author'] = data.get('chapters-' + str(i) + '-author',False)
-            chapter_details['publisher'] = data.get('chapters-' + str(i) + '-publisher',False)
-            chapter_details['date_of_publisher'] = data.get('chapters-' + str(i) + '-date_of_publisher',False)
-            chapter_details['isbn_issn'] = data.get('chapters-' + str(i) + '-number',False)
+            chapter_details['book_title'] = data.get('chapters' + str(i) + '-book_title',False)
+            chapter_details['chapter'] = data.get('chapters' + str(i) + '-chapter',False)    
+            chapter_details['author'] = data.get('chapters' + str(i) + '-author',False)
+            chapter_details['publisher'] = data.get('chapters' + str(i) + '-publisher',False)
+            chapter_details['date_of_publisher'] = data.get('chapters' + str(i) + '-date_of_publisher',False)
+            chapter_details['isbn_issn'] = data.get('chapters' + str(i) + '-number',False)
+            chapter_details['supporting_documents'] = request.FILES['chapters'+str(i)+'-file']
             chapter_details['applicant'] = Applicant.objects.get(application_no=application_number)
-            Chapters.objects.create(**chapter_details)
+            chapters_data.append(chapter_details)
         # Newspapers Articles
-        num_of_news_articles_records = len(list(filter(lambda s: 'chapters' in s, list(data.keys()))))
+        num_of_news_articles_records = len(list(filter(lambda s: 'news_articles' in s, list(data.keys()))))
+        news_articles_data = []
         for i in range(1, num_of_news_articles_records+1):
             news_articles_details = {}
-            news_articles_details['article_title'] = data.get('news_articles-' + str(i) + '-article_title',False)
-            news_articles_details['journal_name'] = data.get('news_articles-' + str(i) + '-journal_name',False)
-            news_articles_details['author'] = data.get('news_articles-' + str(i) + '-author',False)
-            news_articles_details['date_published'] = data.get('news_articles-' + str(i) + '-date_published',False)
-            news_articles_details['referred'] = data.get('news_articles-' + str(i) + '-referred',False)
-            news_articles_details['level'] = data.get('news_articles-' + str(i) + '-level',False)
-            news_articles_details['naas'] = data.get('news_articles-' + str(i) + '-naas',False)
-            news_articles_details['isbn_issn'] = data.get('news_articles-' + str(i) + '-number',False)
+            news_articles_details['article_title'] = data.get('news_articles' + str(i) + '-article_title',False)
+            news_articles_details['journal_name'] = data.get('news_articles' + str(i) + '-journal_name',False)
+            news_articles_details['author'] = data.get('news_articles' + str(i) + '-author',False)
+            news_articles_details['date_published'] = data.get('news_articles' + str(i) + '-date_published',False)
+            news_articles_details['referred'] = data.get('news_articles' + str(i) + '-referred',False)
+            news_articles_details['level'] = data.get('news_articles' + str(i) + '-level',False)
+            news_articles_details['naas'] = data.get('news_articles' + str(i) + '-naas',False)
+            news_articles_details['isbn_issn'] = data.get('news_articles' + str(i) + '-number',False)
+            news_articles_details['supporting_documents'] = request.FILES['news_articles'+str(i)+'-file']
             news_articles_details['applicant'] = Applicant.objects.get(application_no=application_number)
-            NewspaperArticle.objects.create(**news_articles_details)
+            news_articles_data.append(news_articles_details)
         # Seminar Articles
-        num_of_seminar_articles =  len(list(filter(lambda s: 'chapters' in s, list(data.keys()))))
+        num_of_seminar_articles =  len(list(filter(lambda s: 'semi_articles' in s, list(data.keys()))))
+        seminar_articles_data = []
         for i in range(1,num_of_seminar_articles+1):
             seminar_articles_details = {}
-            seminar_articles_details['article_title'] = data.get('semi_articles-' + str(i) + '-article_title',False)
-            seminar_articles_details['seminar_subject'] = data.get('semi_articles-' + str(i) + '-seminar_subject',False)
-            seminar_articles_details['location'] = data.get('semi_articles-' + str(i) + '-location',False)
-            seminar_articles_details['duration'] = data.get('semi_articles-' + str(i) + '-duration',False)
-            seminar_articles_details['published'] = data.get('semi_articles-' + str(i) + '-published',False)
+            seminar_articles_details['article_title'] = data.get('semi_articles' + str(i) + '-article_title',False)
+            seminar_articles_details['seminar_subject'] = data.get('semi_articles' + str(i) + '-seminar_subject',False)
+            seminar_articles_details['location'] = data.get('semi_articles' + str(i) + '-location',False)
+            seminar_articles_details['duration'] = data.get('semi_articles' + str(i) + '-to',False) - data.get('semi_articles' + str(i) + '-from',False)
+            seminar_articles_details['published'] = data.get('semi_articles' + str(i) + '-published',False)
+            seminar_articles_details['supporting_documents'] = request.FILES['semi_articles'+str(i)+'-file']
             seminar_articles_details['applicant'] = Applicant.objects.get(application_no=application_number)
-            SeminarArticles.objects.create(**seminar_articles_details)                   
+            seminar_articles_data.append(seminar_articles_details)
+                             
         #     professional_details['organisation'] = data.get('org' + str(i),False)
         #     professional_details['designation']  = data.get('desig' + str(i),False)
         #     professional_details['from_year'] = data.get('proffrom' + str(i),False)
@@ -235,17 +260,25 @@ def submission_form(request):
         # #handle_uploaded_file(pic, application_number, 'pic')
         # #handle_uploaded_file(cert, application_number, 'certificate')
         # #Storing data
-        # # Applicant.objects.create(**applicant_data)
-        # # academic_details['applicant'] = Applicant.objects.get(application_no=application_number)
-        # # for academic_details in academic_data:
-        # #     Academic_detail.objects.create(**academic_details)
+        # Applicant.objects.create(**applicant_data)
+        academic_details['applicant'] = Applicant.objects.get(application_no=application_number)
+        for academic_details in academic_data:
+            EducationalQualifications.objects.create(**academic_details)
         
-        # # professional_details['applicant'] = Applicant.objects.get(application_no=application_number)
-        # # for professional_details in professional_data:
-        # #     Professional_detail.objects.create(**professional_details) 
-        
-        # # other_details['applicant'] = Applicant.objects.get(application_no=application_number)
-        # # Other_important_details.objects.create(**other_details)
-
+        professional_details['applicant'] = Applicant.objects.get(application_no=application_number)
+        for professional_details in professional_data:
+            EmploymentExp.objects.create(**professional_details) 
+        books_details['applicant'] = Applicant.objects.get(application_no=application_number)
+        for books_details in books_data:
+            Books.objects.create(**books_details)
+        chapter_details['applicant'] = Applicant.objects.get(application_no=application_number)
+        for chapter_details in chapters_data:
+            Chapters.objects.create(**chapter_details)
+        news_articles_details['applicant'] = Applicant.objects.get(application_no=application_number)
+        for news_articles_details in news_articles_data:
+            NewspaperArticle.objects.create(**news_articles_details)
+        seminar_articles_details['applicant'] = Applicant.objects.get(application_no=application_number)
+        for seminar_articles_details in seminar_articles_data:
+            SeminarArticles.objects.create(**seminar_articles_details)    
         return render(request, 'recruitment/form.html',{'message':'Congrats! Your application number is '+ application_number})
     return render(request, 'recruitment/form.html', {})
