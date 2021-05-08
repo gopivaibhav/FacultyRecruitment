008a80d64333380b667eb241f66d144842429f08
Cronus1007 <lit2019058@iiitl.ac.in>
2021-05-08 23:44:35 +0530

    message

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
                 path('', views.home, name='home'),
                 path('login/', views.login , name='login'),
                 path('submit/', views.submission_form, name='submission_form'),
-                path('admin/',views.admin,name="Admin"),
-                path('admin/user/<str:application_number>',views.viewMore,name="View More")
+                path('admin/',admin.site.urls)
+                # path('admin/user/<str:application_number>',views.viewMore,name="View More")
             ]
