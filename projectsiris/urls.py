175ada9a3a6b1790fcb39e75250f7a46c410f9ea
Cronus1007 <lit2019058@iiitl.ac.in>
2021-06-07 11:41:01 +0530

    admin fir se aaa gya

--- projectsiris/urls.py
+++ projectsiris/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 
 urlpatterns = [
     path('', include("recruitment.urls")),
-    path('admin/',admin.site.urls),
     path('accounts/' , include("allauth.urls"))
 ]
 
--- recruitment/urls.py
+++ recruitment/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
                 path('', views.home, name='home'),
-                # path('admin/', views.adminLogin, name="login"),
+                path('admin/', views.adminLogin, name="login"),
                 path('admin/profile/', views.admin, name="admin"),
                 path('accounts/profile/', views.profile, name="profile"),
                 # path('accounts/logout/', views.logout, name="logout"),
