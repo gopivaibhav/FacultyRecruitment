4b47ea8647a71c33f0098dd84a520b1a1aa58cc1
Ayush Jain <ayushjaink8@gmail.com>
2021-08-05 12:07:48 +0000

    message

--- projectsiris/settings.py
+++ projectsiris/settings.py
@@ -24,8 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# chutiye ne env hi nhi diya
+
 # SECRET_KEY = 'tcwkab1w6!h&gc+iu0v5yf)9kr6@fz*eyt=#1*$_zb&+s1pg0@'
+
 # for local System:
 # SECRET_KEY =  str(dotenv_values(".env")["SECRET_KEY"])
 
@@ -167,9 +168,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = str(os.getenv("EMAIL_HOST_USER"))
 EMAIL_HOST_PASSWORD = str(os.getenv("EMAIL_HOST_PASSWORD"))
 
-# # chutiye ne env hi nhi diya hai
-# EMAIL_HOST_USER = "noreply@iiitl.ac.in"
-# EMAIL_HOST_PASSWORD = "adminrecruitment@123"
+#
+# EMAIL_HOST_USER = "noreplyiiitl@gmail.com"
+# EMAIL_HOST_PASSWORD = "adminrecruitment"
 
 
 # for Local:
