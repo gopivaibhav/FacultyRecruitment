1f2fe4d3063e81e93d840aff574eed5bf1c1ddbb
Cronus1007 <lit2019058@iiitl.ac.in>
2021-11-24 23:41:38 +0530

    message

--- projectsiris/settings.py
+++ projectsiris/settings.py
@@ -158,9 +158,9 @@ USE_TZ = True
 
 
 # email send for confirmations
-
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
 # for Deployment:
